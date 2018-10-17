c8265603be13e62a168a2de15c7851e8c5537428
prabeen <prabeensoreng2119@gmail.com>
2018-10-17 13:01:25 +0530

    retrieving data from firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firebase-demo';
+  // courses: Observable<any[]>;
+  courses: any[];
+  constructor(
+    db: AngularFireDatabase
+  ) {
+    db.list('courses').valueChanges()
+      .subscribe(courses => {
+        this.courses = courses;
+        console.log(this.courses);
+      });
+  }
 }
